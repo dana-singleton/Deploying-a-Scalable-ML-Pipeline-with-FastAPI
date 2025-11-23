0d9ff4f382b96dcdf38695ab7682506b860c95a1
Dana Singleton <dsin190@wgu.edu>
2025-11-23 21:00:47 +0000

    Implement FastAPI API in main.py

--- main.py
+++ main.py
@@ -26,24 +26,22 @@ class Data(BaseModel):
     hours_per_week: int = Field(..., example=40, alias="hours-per-week")
     native_country: str = Field(..., example="United-States", alias="native-country")
 
-path = None # TODO: enter the path for the saved encoder 
-encoder = load_model(path)
+# Completed: entered paths for saved encoder and saved model.
+encoder = load_model("model/encoder.pkl")
+model = load_model("model/model.pkl")
 
-path = None # TODO: enter the path for the saved model 
-model = load_model(path)
+# Completed: create a RESTful API using FastAPI
+app = FastAPI() # updated code here for FastAPI
 
-# TODO: create a RESTful API using FastAPI
-app = None # your code here
-
-# TODO: create a GET on the root giving a welcome message
+# Completed: create a GET on the root giving a welcome message
 @app.get("/")
 async def get_root():
     """ Say hello!"""
-    # your code here
-    pass
+    # added my code here
+    return {"message": "Welcome to the Census Income Prediction API!"}
 
 
-# TODO: create a POST on a different path that does model inference
+# Completed: create a POST on a different path that does model inference
 @app.post("/data/")
 async def post_inference(data: Data):
     # DO NOT MODIFY: turn the Pydantic model into a dict.
@@ -65,10 +63,17 @@ async def post_inference(data: Data):
         "native-country",
     ]
     data_processed, _, _, _ = process_data(
-        # your code here
+        # updated the  code below
         # use data as data input
         # use training = False
         # do not need to pass lb as input
-    )
-    _inference = None # your code here to predict the result using data_processed
+        data,
+        categorical_features=cat_features,
+        label=None,
+        training=False,
+        encoder=encoder,
+        lb=None
+        )
+    # completed code below to predict the result using data_processed
+    _inference = inference(model, data_processed)
     return {"result": apply_label(_inference)}
